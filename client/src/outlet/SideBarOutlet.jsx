0aab1e469d15f6778b142e129a34e99d22f42e29
rappire <rappire@gmail.com>
2024-10-26 01:11:35 +0900

    feat: Outlet 사용하게 변경

--- client/src/outlet/SideBarOutlet.jsx
+++ client/src/outlet/SideBarOutlet.jsx
@@ -27,7 +27,6 @@ const Content = styled.div`
   align-items: center;
   justify-content: center;
   padding: 20px;
-  z-index: 1;
 `;
 
 export default SideBarOutlet;
--- client/src/pages/Couple.tsx
+++ client/src/pages/Couple.tsx
@@ -4,30 +4,7 @@ import tw from "tailwind-styled-components";
 import Sidebar from "../components/Sidebar";
 
 const Couple = () => {
-    return (
-        <MainContainer>
-            <Sidebar/>
-            <Content>
-                커플 페이지
-            </Content>
-        </MainContainer>
-    );
+  return <div>커플 페이지</div>;
 };
-
-const MainContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
-`;
-
-const Content = styled.div`
-    flex: 1;
-    display: flex;
-    background-color: #f9fafb;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-`;
-
 
 export default Couple;
