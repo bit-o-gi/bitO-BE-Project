8cb2a3059a258a6d169038aa4bb0f2d0d5b3e9b6
심봉교 <bonggyo.sim@ipageon.com>
2024-12-10 08:42:52 +0900

    feat : Swagger 설정

--- src/main/java/bit/anniversary/controller/AnController.java
+++ src/main/java/bit/anniversary/controller/AnController.java
@@ -11,6 +11,8 @@ import org.springframework.stereotype.Controller;
 import bit.anniversary.dto.AnReqDto;
 import bit.anniversary.dto.AnResDto;
 import bit.anniversary.service.AnService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
@@ -21,45 +23,52 @@ public class AnController {
 
 	private final AnService anniversaryService;
 
-	// 기념일 생성
+	//NOTE: 기념일 생성
+	@Operation(summary = "기념일 생성", description = "새로운 기념일을 생성합니다.")
 	@MutationMapping
-	public AnResDto createAnniversary(@Argument("anDto") AnReqDto anReqDto) {
+	public AnResDto createAnniversary(@Parameter(description = "기념일 요청 데이터") @Argument("anDto") AnReqDto anReqDto) {
 		log.info("anReqDto ==> {}",anReqDto);
 		return anniversaryService.createAnniversary(anReqDto);
 	}
 
-	// 기념일 업데이트
+	//NOTE: 기념일 업데이트
+	@Operation(summary = "기념일 수정", description = "특정 ID를 가진 기념일을 수정합니다.")
 	@MutationMapping
-	public AnResDto updateAnniversary(@Argument("id") Long id, @Argument("anDto") AnReqDto anReqDto) {
+	public AnResDto updateAnniversary(@Parameter(description = "수정할 기념일 ID") @Argument("id") Long id, @Parameter(description = "수정된 기념일 데이터")  @Argument("anDto") AnReqDto anReqDto) {
 		return anniversaryService.updateAnniversary(id, anReqDto);
 	}
 
-	// 기념일 삭제
+	//NOTE: 기념일 삭제
+	@Operation(summary = "기념일 삭제", description = "특정 ID를 가진 기념일을 삭제합니다.")
 	@MutationMapping
-	public Boolean deleteAnniversary(@Argument("id") Long id) {
+	public Boolean deleteAnniversary(@Parameter(description = "삭제할 기념일 ID") @Argument("id") Long id) {
 		anniversaryService.deleteAnniversary(id);
 		return true;
 	}
 
-	// 특정 기념일 조회
+	//NOTE: 특정 기념일 조회
+	@Operation(summary = "특정 기념일 조회", description = "특정 ID를 가진 기념일 정보를 조회합니다.")
 	@QueryMapping
 	public AnResDto getAnniversary(@Argument Long id) {
 		return anniversaryService.getAnniversary(id);
 	}
 
-	// 날짜 범위로 기념일 목록 조회
+	//NOTE: 날짜 범위로 기념일 목록 조회
+	@Operation(summary = "날짜 범위로 기념일 조회", description = "특정 날짜 범위 내의 기념일 목록을 조회합니다.")
 	@QueryMapping
-	public List<AnResDto> getAnniversariesInRange(@Argument LocalDateTime startDate, @Argument LocalDateTime endDate) {
+	public List<AnResDto> getAnniversariesInRange(@Parameter(description = "조회 시작 날짜")  @Argument LocalDateTime startDate, @Parameter(description = "조회 종료 날짜") @Argument LocalDateTime endDate) {
 		return anniversaryService.findAnniversariesInRange(startDate, endDate);
 	}
 
-	// 한 달 기념일 목록 조회
+	//NOTE: 한 달 기념일 목록 조회
+	@Operation(summary = "한 달 기념일 조회", description = "앞으로 한 달 동안의 기념일 목록을 조회합니다.")
 	@QueryMapping
 	public List<AnResDto> getMonthlyAnniversaries() {
 		return anniversaryService.findAnniversariesInRange(LocalDateTime.now(), LocalDateTime.now().plusMonths(1));
 	}
 
-	// 1년 기념일 목록 조회
+	//NOTE: 1년 기념일 목록 조회
+	@Operation(summary = "1년 기념일 조회", description = "앞으로 1년 동안의 기념일 목록을 조회합니다.")
 	@QueryMapping
 	public List<AnResDto> getYearlyAnniversaries() {
 		return anniversaryService.findAnniversariesInRange(LocalDateTime.now(), LocalDateTime.now().plusYears(1));
