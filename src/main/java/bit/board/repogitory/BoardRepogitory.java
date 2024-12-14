5fbb8091b38012fd24ab731d0448485985801d96
parkSeryu <pjh971212@gmail.com>
2024-12-14 15:47:25 +0900

    chore: board 삭제

--- src/main/java/bit/board/repogitory/BoardRepogitory.java
+++
@@ -1,8 +0,0 @@
-package bit.board.repogitory;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import bit.board.entity.Board;
-
-public interface BoardRepogitory extends JpaRepository<Board, Long> {
-}
